77865404b8565ee4918a70be6e40243037131bbc
Caio Theodoro <dev.caiotheodoro@gmail.com>
2024-08-12 23:20:31 -0300

    Add files via upload

---
+++ ARDUINO_GATEWAY_TRIO.ino
@@ -0,0 +1,134 @@
+#include <SPI.h>
+#include <EEPROM.h>
+#include "RF24.h"
+
+#define CE_PIN 7
+#define CSN_PIN 8
+#define SERVICE_KEY 123123123  
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+uint8_t address[][6] = { "00001", "00002", "00003" };
+
+String encryptPassword(String password) {
+  String encrypted = "";
+  for (int i = 0; i < password.length(); i++) {
+    encrypted += char(password[i] ^ SERVICE_KEY);
+  }
+  return encrypted;
+}
+
+void writeStringToEEPROM(int addrOffset, const String &strToWrite) {
+  byte len = strToWrite.length();
+  EEPROM.write(addrOffset, len);
+  for (int i = 0; i < len; i++) {
+    EEPROM.write(addrOffset + 1 + i, strToWrite[i]);
+  }
+}
+
+String readStringFromEEPROM(int addrOffset) {
+  int newStrLen = EEPROM.read(addrOffset);
+  char data[newStrLen + 1];
+  for (int i = 0; i < newStrLen; i++) {
+    data[i] = EEPROM.read(addrOffset + 1 + i);
+  }
+  data[newStrLen] = '\0'; 
+  return String(data);
+}
+
+int findLoginInEEPROM(String login) {
+  int address = 0;
+  while (address < EEPROM.length()) {
+    String storedData = readStringFromEEPROM(address);
+    int colonIndex = storedData.indexOf(':');
+    String storedLogin = storedData.substring(0, colonIndex);
+
+    if (storedLogin == login) {
+      return address;
+    }
+
+    address += storedData.length() + 1;
+  }
+  return -1; 
+}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {}
+
+  if (!radio.begin()) {
+    Serial.println(F("radio hardware is not responding!!"));
+    while (1) {}
+  }
+
+  Serial.println(F("RF24 Gateway - Handling Register/Login"));
+
+  radio.setPALevel(RF24_PA_LOW);
+
+  radio.openReadingPipe(1, address[0]);
+
+  radio.startListening();
+}
+
+void loop() {
+  if (radio.available()) {
+    char receivedPayload[32] = {0};
+    radio.read(&receivedPayload, sizeof(receivedPayload));
+
+    Serial.print(F("Received: "));
+    Serial.println(receivedPayload);
+
+    String receivedString = String(receivedPayload);
+    int firstColon = receivedString.indexOf(':');
+    int secondColon = receivedString.indexOf(':', firstColon + 1);
+
+    String type = receivedString.substring(0, firstColon);
+    String login = receivedString.substring(firstColon + 1, secondColon);
+    String password = receivedString.substring(secondColon + 1);
+    
+    char response[32] = {0};
+
+    if (type == "register") {
+      int startAddress = findLoginInEEPROM(login);
+      if (startAddress != -1) {
+        strcpy(response, "register_failed");
+        Serial.println(F("Registration failed. Login already taken."));
+      } else {
+        String encryptedPassword = encryptPassword(password);
+        startAddress = EEPROM.length();  
+        writeStringToEEPROM(startAddress, login + ":" + encryptedPassword);
+
+        strcpy(response, "register_success");
+        Serial.println(F("Registration successful and data saved to EEPROM."));
+      }
+    } else if (type == "login") {
+      int startAddress = findLoginInEEPROM(login);
+      if (startAddress != -1) {
+        String storedData = readStringFromEEPROM(startAddress);
+        int storedColon = storedData.indexOf(':');
+        String storedEncryptedPassword = storedData.substring(storedColon + 1);
+        String decryptedStoredPassword = encryptPassword(storedEncryptedPassword);
+
+        if (password == decryptedStoredPassword) {
+          strcpy(response, "login_success");
+          Serial.println(F("Login successful."));
+        } else {
+          strcpy(response, "login_failed");
+          Serial.println(F("Login failed. Incorrect password."));
+        }
+      } else {
+        strcpy(response, "login_failed");
+        Serial.println(F("Login failed. Login not found."));
+      }
+    }
+
+    // Send response back
+    radio.stopListening();  
+    radio.flush_rx();  
+    radio.flush_tx();  
+    radio.write(&response, sizeof(response)); 
+    radio.startListening();  
+  }
+
+  delay(100);
+}
---
+++ ARDUINO_REGISTER.ino
@@ -0,0 +1,84 @@
+#include <SPI.h>
+#include "RF24.h"
+
+#define CE_PIN 7
+#define CSN_PIN 8
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+uint8_t address[] = "00001";  
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {}
+
+  if (!radio.begin()) {
+    Serial.println(F("radio hardware is not responding!!"));
+    while (1) {}
+  }
+
+  Serial.println(F("RF24 Register Arduino"));
+
+  radio.setPALevel(RF24_PA_LOW);
+
+  radio.openWritingPipe(address);
+  radio.openReadingPipe(1, address);
+
+  radio.stopListening();
+}
+
+void loop() {
+  Serial.println(F("Enter login for registration: "));
+  while (!Serial.available()) {}
+  String login = Serial.readStringUntil('\n');
+  login.trim();
+
+  Serial.println(F("Enter password for registration: "));
+  while (!Serial.available()) {}
+  String password = Serial.readStringUntil('\n');
+  password.trim();
+
+  String payload = "register:" + login + ":" + password;
+
+  char payloadArray[32];
+  payload.toCharArray(payloadArray, 32);
+
+  radio.flush_tx();
+  bool report = radio.write(&payloadArray, sizeof(payloadArray));
+
+  if (report) {
+    Serial.println(F("Registration request sent successfully!"));
+    Serial.println(payload);
+
+    radio.startListening();
+
+    unsigned long startedWaitingAt = millis();
+    bool timeout = false;
+    while (!radio.available()) {
+      if (millis() - startedWaitingAt > 3000) {  
+        timeout = true;
+        break;
+      }
+    }
+
+    if (timeout) {
+      Serial.println(F("Response timeout."));
+    } else {
+      char response[32] = {0};
+      radio.read(&response, sizeof(response));
+
+      if (String(response) == "register_success") {
+        Serial.println(F("Registration successful!"));
+      } else if (String(response) == "register_failed") {
+        Serial.println(F("Registration failed. Login already taken."));
+      }
+    }
+
+    radio.flush_rx();
+    radio.stopListening();
+  } else {
+    Serial.println(F("Registration request failed or timed out"));
+  }
+
+  delay(1000);
+}
